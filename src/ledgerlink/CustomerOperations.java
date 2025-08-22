6b2b54461c73ac6bd0039f6619b0b3f5b2a18d0f
ShashankMk031 <shashankmk031@gmail.com>
2025-08-23 02:22:18 +0530

    Modularized customer viewing logic into CustomerOperations class for better separation of concerns.

--- src/ledgerlink/CustomerOperations.java
+++ src/ledgerlink/CustomerOperations.java
@@ -1,1 +1,39 @@
-package src.ledgerlink;
+package ledgerlink;
+
+import java.sql.Connection;
+import java.sql.ResultSet; 
+import java.sql.Statement; 
+
+public class CustomerOperations {
+    // Prints all customer from the database 
+    public static void viewAllCustomers(){
+        Connection conn = null;
+        Statement stmt = null; 
+        ResultSet rs = null; 
+
+        try{
+            conn = DBUtils.getConnection(); 
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM Customer");
+
+            System.out.println("CustomerID | Name | Email | Phone | Address | CreatedAt"); 
+            System.out.println("-------------------------------------------------------------");
+
+            while (rs.next()){
+                int id = rs.getInt("customer_id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                String address = rs.getString("address");
+                String createdAt = rs.getString("created_at");
+
+                System.out.printf("%10d | %s | %s | %s | %s | %s\n", id, name, email, phone, address, createdAt);
+            }
+
+        } catch (Exception e){
+            System.err.println("Error retrieving customers: " + e.getMessage());
+        } finally {
+            DBUtils.closeConnection(conn);
+        }
+    }
+}
