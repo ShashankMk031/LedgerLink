72f2f25a108f382cd8334cc6353362889ebdaa89
ShashankMk031 <shashankmk031@gmail.com>
2025-09-02 19:55:01 +0530

    Adding the Loan servie into the application

--- src/ledgerlink/dao/LoanDAO.java
+++ src/ledgerlink/dao/LoanDAO.java
@@ -1,1 +1,79 @@
 package ledgerlink.dao;
+
+
+import ledgerlink.model.Loan; 
+import ledgerlink.util.DBUtil; 
+
+import java.sql.*; 
+import java.time.LocalDateTime; 
+
+public class LoanDAO { 
+
+    public boolean insert(Loan l) throws SQLException { 
+
+        String sql = "INSERT INTO loan(customerId, targetAccountId, principal, annualRate, termMonths, status, appliedAt)" + "VALUES (?, ?, ?, ?, ?, ?, ?)"; 
+        try (Connection conn  = DBUtil.getConnection(); 
+            PreparedStatement ps = conn.prepareStatement(sql)) { 
+                ps.setInt(1, l.getCustomerId()); 
+                if(l.getTargetAccountId() == null) ps.setNull(2, Types.INTEGER); 
+                else ps.setInt(2, l.getTargetAccountId()); 
+                ps.setDouble(3, l.getPrincipal()); 
+                ps.setDouble(4, l.getAnnualRate()); 
+                ps.setInt(5, l.getTermMonths());  
+                ps.setString(6, l.getStatus()); 
+                ps.setTimestamp(7, Timestamp.valueOf(l.getAppliedAt()));
+
+                int rows = ps.executeUpdate(); 
+                if(rows == 1) { 
+                    try (ResultSet keys = ps.getGeneratedKeys()) { 
+                        if (keys.next()) l.setLoanId(keys.getInt(1)); 
+                    } 
+                    return true; 
+            } 
+                return false;
+        }
+    } 
+
+    public Loan findById(int loanId) throws SQLException { 
+        String sql = "SELECT loanId, customerId, targetAccountId, principal, annualRate, termMonths, staatus, appliedAt, approvedAt, disbursedAt " + "FROM loan WHERE loanId = ? ";
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, loanId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) return map(rs);
+            }
+            return null;
+        }
+    } 
+
+
+    public boolean updateStatusAndTime (int loanId, String status, String timeColumn, LocalDateTime time) throws SQLException {
+        String sql = "UPDATE loan SET status = ? , " + timeColumn + " = ? WHERE loanId = ?"; 
+
+        try (Connection conn = DBUtil.getConnection(); 
+            PreparedStatement ps = conn.prepareStatement(sql)) { 
+                ps.setString(1, status); 
+                ps.setTimestamp(2, time == null ? null : Timestamp.valueOf(time)); 
+                ps.setInt(3, loanId); 
+                return ps.executeUpdate() == 1; 
+        }
+    } 
+
+    private Loan map(ResultSet rs) throws SQLException { 
+        Timestamp a = rs.getTimestamp("appliedAt"); 
+        Timestamp ap = rs.getTimestamp("approvedAt"); 
+        Timestamp d = rs.getTimestamp("disbursedAt"); 
+        return new Loan(
+                rs.getInt("loanId"),
+                rs.getInt("customerId"),
+                (Integer) rs.getObject("targetAccountId"),
+                rs.getDouble("principal"),
+                rs.getDouble("annualRate"),
+                rs.getInt("termMonths"),
+                rs.getString("status"),
+                a == null ? null : a.toLocalDateTime(),
+                ap == null ? null : ap.toLocalDateTime(),
+                d == null ? null : d.toLocalDateTime()
+        );
+    }
+ }
---
+++ src/ledgerlink/model/Loan.java
@@ -0,0 +1,73 @@
+/* Apply: create a loan request tied to a customer and a target account (where funds will be disbursed) with principal, rate, term, and status=PENDING.
+
+Approve: set status=APPROVED with an approval timestamp.
+
+Disburse: move funds into the borrower account in a single DB transaction and set status=DISBURSED.
+
+ */ 
+
+
+// ensuress minimal fields 
+
+package ledgerlink.model; 
+
+import java.time.LocalDateTime; 
+
+public class Loan { 
+    private int loadId; 
+    private int customerId; 
+    private Integer tragetAccountId; 
+    private double principal ; 
+    private double annualRate; 
+    private int termMonths; 
+    private String status;   // Pending, Approved, disbursed , Rejected, Closed 
+    private LocalDateTime appliedAt; 
+    private LocalDateTime approvedAt; 
+    private LocalDateTime disbursedAt; 
+    
+
+    public Loan() {} 
+
+    public Loan(int loadId, int customerId, Integer targetAccountId, double principal, double annualRate , int termMonths, String status, LocalDateTime appliedAt, LocalDateTime approvedAt, LocalDateTime disbursedAt) {
+        this.loadId = loadId; 
+        this.customerId = customerId; 
+        this.tragetAccountId = targetAccountId; 
+        this.principal = principal; 
+        this.annualRate = annualRate; 
+        this.termMonths = termMonths; 
+        this.status = status; 
+        this.appliedAt = appliedAt; 
+        this.approvedAt = approvedAt; 
+        this.disbursedAt = disbursedAt;
+    } 
+
+    public int getLoanId() { return loadId; } 
+    public void setLoanId(int loanId) { this.loadId = loanId; } 
+
+    public int getCustomerId() { return customerId; } 
+    public void setCustomerId(int customerId) { this.customerId = customerId; } 
+
+    public Integer getTargetAccountId() { return tragetAccountId; } 
+    public void setTargetAccountId(Integer targetAccountId) { this.tragetAccountId = targetAccountId; } 
+
+    public double getPrincipal() { return principal; } 
+    public void setPrincipal(double principal) { this.principal = principal; } 
+
+    public double getAnnualRate() { return annualRate; } 
+    public void setAnnualRate(double annualRate) { this.annualRate = annualRate; } 
+
+    public int getTermMonths() { return termMonths; } 
+    public void setTermMonths(int termMonths) { this.termMonths = termMonths; } 
+
+    public String getStatus() { return status; } 
+    public void setStatus(String status) { this.status = status; } 
+
+    public LocalDateTime getAppliedAt() { return appliedAt; } 
+    public void setAppliedAt(LocalDateTime appliedAt) { this.appliedAt = appliedAt; } 
+
+    public LocalDateTime getApprovedAt() { return approvedAt; } 
+    public void setApprovedAt(LocalDateTime approvedAt) { this.approvedAt = approvedAt; } 
+
+    public LocalDateTime getDisbursedAt() { return disbursedAt; }   
+    public void setDisbursedAt(LocalDateTime disbursedAt) { this.disbursedAt = disbursedAt; }
+} 
